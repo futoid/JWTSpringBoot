d44d317ea07fd4e16c4412575610213a9bf496b9
futoid <aliekmandal24@gmail.com>
2025-04-10 09:03:07 +0530

    uddate: password encoder

--- src/main/java/com/aliek/JwtSecurity/service/UserService.java
+++ src/main/java/com/aliek/JwtSecurity/service/UserService.java
@@ -3,6 +3,7 @@ package com.aliek.JwtSecurity.service;
 import com.aliek.JwtSecurity.models.Users;
 import com.aliek.JwtSecurity.respository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,7 +12,10 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
+
     public Users saveUser(Users user) {
+        user.setPassword(encoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
